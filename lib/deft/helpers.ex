5e6c7a01ef74a0a227b4d64f897f10e4090b9a64
Quinn Wilton <quinn@quinnwilton.com>
2022-07-01 14:37:10 -0700

    Add more helpers for computing types in context and working with multiple types

--- lib/deft/helpers.ex
+++ lib/deft/helpers.ex
@@ -7,18 +7,27 @@ defmodule Deft.Helpers do
     Macro.update_meta(e, &Keyword.delete(&1, :__deft_type__))
   end
 
-  def type_of({_, meta, _}) do
+  def type_of({_, meta, _} = e) do
     case Keyword.fetch(meta, :__deft_type__) do
       :error ->
-        nil
+        raise Deft.MissingTypeError, expr: e
 
       {:ok, type} ->
         type
     end
   end
 
+  def types_of(es) do
+    Enum.map(es, &type_of/1)
+  end
+
   def subtype?(t1, t2) do
     Deft.Type.subtype?(t1, t2)
+  end
+
+  def subtypes?(t1s, t2s) do
+    Enum.zip(t1s, t2s)
+    |> Enum.all?(fn {t1, t2} -> subtype?(t1, t2) end)
   end
 
   def compute_type(e, env) do
@@ -35,9 +44,35 @@ defmodule Deft.Helpers do
 
   def compute_and_erase_type(e, env) do
     e = local_expand(e, env)
+    erased = delete_annotation(e)
     type = type_of(e)
 
-    {e, type}
+    {erased, type}
+  end
+
+  def compute_and_erase_types(es, env) do
+    es
+    |> Enum.map(&compute_and_erase_type(&1, env))
+    |> Enum.unzip()
+  end
+
+  def compute_and_erase_type_in_context(e, context, env) do
+    e =
+      Enum.reduce(context, e, fn {x_f, x_m, x_a} = x, acc ->
+        Macro.postwalk(acc, fn {a_f, a_m, a_a} = a ->
+          # TODO: Is this necessary?
+          x_counter = Keyword.take(x_m, [:counter])
+          a_counter = Keyword.take(a_m, [:counter])
+
+          if x_f == a_f and x_counter == a_counter and x_a == a_a do
+            annotate(a, type_of(x))
+          else
+            a
+          end
+        end)
+      end)
+
+    compute_and_erase_type(e, env)
   end
 
   def local_expand(e, env) do
