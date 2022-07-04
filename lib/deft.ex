cf0cb703784f93a63c6745ee5e695488d002aedd
Quinn Wilton <quinn@quinnwilton.com>
2022-07-03 18:17:05 -0700

    Move AST rewriting to Deft.Compiler

--- lib/deft.ex
+++ lib/deft.ex
@@ -1,9 +1,8 @@
 defmodule Deft do
-  alias Deft.AST
-  alias Deft.Annotations
+  alias Deft.Compiler
 
   defmacro compile(do: block) do
-    block = rewrite(block)
+    block = Compiler.compile(block)
 
     {block, _type} =
       Deft.Helpers.compute_and_erase_types(
@@ -15,7 +14,7 @@ defmodule Deft do
   end
 
   defmacro type(do: block) do
-    block = rewrite(block)
+    block = Compiler.compile(block)
 
     {_block, type} =
       Deft.Helpers.compute_and_erase_types(
@@ -24,116 +23,5 @@ defmodule Deft do
       )
 
     Macro.escape(type)
-  end
-
-  def rewrite({:__block__, meta, exprs}) do
-    exprs = Enum.map(exprs, &rewrite/1)
-
-    AST.Block.new(exprs, meta)
-  end
-
-  def rewrite({:fn, fn_meta, [{:->, arrow_meta, [args, body]}]}) do
-    args = Enum.map(args, &rewrite_fn_arg/1)
-    body = rewrite(body)
-
-    AST.Fn.new(body, args, fn_meta, arrow_meta)
-  end
-
-  def rewrite({{:., fun_meta, [fun]}, args_meta, args}) do
-    fun = rewrite(fun)
-    args = Enum.map(args, &rewrite/1)
-
-    AST.FnApplication.new(fun, args, fun_meta, args_meta)
-  end
-
-  def rewrite({:if, meta, [predicate, branches]}) do
-    predicate = rewrite(predicate)
-    branches = Keyword.map(branches, &rewrite(elem(&1, 1)))
-
-    AST.If.new(
-      predicate,
-      branches[:do],
-      branches[:else],
-      meta
-    )
-  end
-
-  def rewrite({:cond, cond_meta, [[do: branches]]}) do
-    branches =
-      Enum.map(branches, fn
-        {:->, branch_meta, [[predicate], body]} ->
-          predicate = rewrite(predicate)
-          body = rewrite(body)
-
-          AST.CondBranch.new(predicate, body, branch_meta)
-      end)
-
-    AST.Cond.new(branches, cond_meta)
-  end
-
-  def rewrite({:case, case_meta, [subject, [do: branches]]}) do
-    subject = rewrite(subject)
-
-    branches =
-      Enum.map(branches, fn
-        {:->, branch_meta, [[pattern], body]} ->
-          pattern = rewrite_pattern(pattern)
-
-          AST.CaseBranch.new(pattern, body, branch_meta)
-      end)
-
-    AST.Case.new(subject, branches, case_meta)
-  end
-
-  def rewrite({:=, meta, [pattern, value]}) do
-    pattern = rewrite_pattern(pattern)
-    value = rewrite(value)
-
-    AST.Match.new(pattern, value, meta)
-  end
-
-  def rewrite({:{}, meta, elements}) do
-    elements = Enum.map(elements, &rewrite/1)
-
-    AST.Tuple.new(elements, meta)
-  end
-
-  def rewrite({fst, snd}) do
-    fst = rewrite(fst)
-    snd = rewrite(snd)
-
-    AST.Pair.new(fst, snd)
-  end
-
-  def rewrite(elements) when is_list(elements) do
-    elements = Enum.map(elements, &rewrite/1)
-
-    AST.List.new(elements)
-  end
-
-  def rewrite({name, meta, args}) when is_list(args) do
-    args = Enum.map(args, &rewrite/1)
-
-    AST.LocalCall.new(name, args, meta)
-  end
-
-  def rewrite({name, meta, context}) when is_atom(context) do
-    AST.Local.new(name, context, meta)
-  end
-
-  def rewrite(literal) do
-    literal
-  end
-
-  def rewrite_fn_arg({:"::", meta, [name, annotation]}) do
-    # TODO: handle literals in function heads
-    type = Annotations.parse(annotation)
-    name = rewrite(name)
-
-    AST.Annotation.new(name, type, meta)
-  end
-
-  def rewrite_pattern({name, meta, context}) when is_atom(context) do
-    AST.Local.new(name, context, meta)
   end
 end
---
+++ lib/deft/compiler.ex
@@ -0,0 +1,115 @@
+defmodule Deft.Compiler do
+  alias Deft.AST
+  alias Deft.Annotations
+
+  def compile({:__block__, meta, exprs}) do
+    exprs = Enum.map(exprs, &compile/1)
+
+    AST.Block.new(exprs, meta)
+  end
+
+  def compile({:fn, fn_meta, [{:->, arrow_meta, [args, body]}]}) do
+    args = Enum.map(args, &compile_fn_arg/1)
+    body = compile(body)
+
+    AST.Fn.new(body, args, fn_meta, arrow_meta)
+  end
+
+  def compile({{:., fun_meta, [fun]}, args_meta, args}) do
+    fun = compile(fun)
+    args = Enum.map(args, &compile/1)
+
+    AST.FnApplication.new(fun, args, fun_meta, args_meta)
+  end
+
+  def compile({:if, meta, [predicate, branches]}) do
+    predicate = compile(predicate)
+    branches = Keyword.map(branches, &compile(elem(&1, 1)))
+
+    AST.If.new(
+      predicate,
+      branches[:do],
+      branches[:else],
+      meta
+    )
+  end
+
+  def compile({:cond, cond_meta, [[do: branches]]}) do
+    branches =
+      Enum.map(branches, fn
+        {:->, branch_meta, [[predicate], body]} ->
+          predicate = compile(predicate)
+          body = compile(body)
+
+          AST.CondBranch.new(predicate, body, branch_meta)
+      end)
+
+    AST.Cond.new(branches, cond_meta)
+  end
+
+  def compile({:case, case_meta, [subject, [do: branches]]}) do
+    subject = compile(subject)
+
+    branches =
+      Enum.map(branches, fn
+        {:->, branch_meta, [[pattern], body]} ->
+          pattern = compile_pattern(pattern)
+
+          AST.CaseBranch.new(pattern, body, branch_meta)
+      end)
+
+    AST.Case.new(subject, branches, case_meta)
+  end
+
+  def compile({:=, meta, [pattern, value]}) do
+    pattern = compile_pattern(pattern)
+    value = compile(value)
+
+    AST.Match.new(pattern, value, meta)
+  end
+
+  def compile({:{}, meta, elements}) do
+    elements = Enum.map(elements, &compile/1)
+
+    AST.Tuple.new(elements, meta)
+  end
+
+  def compile({fst, snd}) do
+    fst = compile(fst)
+    snd = compile(snd)
+
+    AST.Pair.new(fst, snd)
+  end
+
+  def compile(elements) when is_list(elements) do
+    elements = Enum.map(elements, &compile/1)
+
+    AST.List.new(elements)
+  end
+
+  def compile({name, meta, args}) when is_list(args) do
+    args = Enum.map(args, &compile/1)
+
+    AST.LocalCall.new(name, args, meta)
+  end
+
+  def compile({name, meta, context}) when is_atom(context) do
+    AST.Local.new(name, context, meta)
+  end
+
+  def compile(literal) do
+    literal
+  end
+
+  def compile_fn_arg({:"::", meta, [name, annotation]}) do
+    # TODO: handle literals in function heads
+    type = Annotations.parse(annotation)
+    name = compile(name)
+
+    AST.Annotation.new(name, type, meta)
+  end
+
+  def compile_pattern({name, meta, context}) when is_atom(context) do
+    AST.Local.new(name, context, meta)
+  end
+end
